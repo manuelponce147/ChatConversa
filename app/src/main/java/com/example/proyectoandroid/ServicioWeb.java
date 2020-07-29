1cfc67a71057ec399a37bce9f76e6c3e56fab3d4
Manuel Ponce <manuelponce147@gmail.com>
2020-07-29 18:30:43 -0400

    UPDATE

--- app/src/main/java/com/example/proyectoandroid/ServicioWeb.java
+++ app/src/main/java/com/example/proyectoandroid/ServicioWeb.java
@@ -1,11 +1,16 @@
 package com.example.proyectoandroid;
 
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.Headers;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
+
 import com.example.proyectoandroid.Constantes;
 
 public interface ServicioWeb {
